70d33dae451ac9c8fd82b99439e8f4c725645b60
tangShunZhi <1046975221@qq.com>
2015-11-16 15:44:46 -0800

    1、项目初始化

---
+++ NewsFromHTML/NewsFromHTML/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsFromHTML
+//
+//  Created by Tsz on 15/11/16.
+//  Copyright © 2015年 Tsz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewsFromHTML/NewsFromHTML/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewsFromHTML
+//
+//  Created by Tsz on 15/11/16.
+//  Copyright © 2015年 Tsz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
