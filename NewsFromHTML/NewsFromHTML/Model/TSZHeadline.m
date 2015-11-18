80170e95a13fc63a4d82132763cbff8c4ecddb4a
tangShunZhi <1046975221@qq.com>
2015-11-17 16:43:29 -0800

    1、使用 kvc实现字典转模型

--- NewsFromHTML/NewsFromHTML/Model/TSZHeadline.m
+++ NewsFromHTML/NewsFromHTML/Model/TSZHeadline.m
@@ -18,9 +18,18 @@
 - (instancetype)initWithDict:(NSDictionary *)dict{
     
     if (self = [super init]) {
-        self.title = dict[@"title"];
-        self.digest = dict[@"digest"];
-        self.imgsrc = dict[@"imgsrc"];
+//        self.title = dict[@"title"];
+//        self.digest = dict[@"digest"];
+//        self.imgsrc = dict[@"imgsrc"];
+        
+        // KVC (key value coding)键值编码
+        // cocoa 的大招，允许间接修改对象的属性值
+        // 第一个参数是字典的数值
+        // 第二个参数是类的属性
+        
+        [self setValue:dict[@"title"] forKey:@"title"];
+        [self setValue:dict[@"digest"] forKey:@"digest"];
+        [self setValue:dict[@"imgsrc"] forKey:@"imgsrc"];
     }
     return self;
 }
