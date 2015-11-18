41971044e20edc159be642fa7b422ccd701dd310
tangShunZhi <1046975221@qq.com>
2015-11-17 16:37:49 -0800

    提交第三方框架

---
+++ NewsFromHTML/NewsFromHTML/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsFromHTML
+//
+//  Created by Tsz on 15/11/16.
+//  Copyright © 2015年 Tsz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
